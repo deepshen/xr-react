3ae1a5a5fc402c74438072923b77f4766389ce60
liuwei <liuwei@doctorwork.com>
2020-08-20 12:42:01 +0800

    fix

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "preset": "ts-jest",
   "collectCoverage": true, /*手机测试覆盖率信息*/
   "collectCoverageFrom": ["<rootDir>/src/components/**/*.{js,jsx,ts,tsx}"],// 搜集哪些文件覆盖率
-  coverageDirectory: '<rootDir>/test/coverage', // 输出覆盖信息文件的目录
+  coverageDirectory: '<rootDir>/src/test/coverage', // 输出覆盖信息文件的目录
   "moduleNameMapper": {
     // 处理webpack resolve.alias匹配
   },
